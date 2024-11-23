3e989bfa1aa9b91caa884c79ab70891529396a74
Vis Artem <artem.visotskiyy@gmail.com>
2024-11-23 22:59:10 +0300

    First Commit

---
+++ project_for_flora/server/db.js
@@ -0,0 +1,10 @@
+const { Sequelize } = require("sequelize")
+
+const db = new Sequelize('flora_zelenograd', 'postgres', 'qwerty22', {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432
+});
+
+
+module.exports = db
